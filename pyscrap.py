1b76cae6fa38bf75dfe227a5fefaf9e78676c1dc
kalyan <kalyan.raparthi.com>
2025-01-26 11:30:01 +0530

    added example for http.client

--- pyscrap.py
+++ pyscrap.py
@@ -4,4 +4,14 @@ AUTHOUR:
 1. KALYAN RAPARTHI / [ qb ], kalyan.raparthi@hotmail.com, GitHub: kalyan-raparthi
 '''
 
-# planned
+import http.client
+
+url = "kalyan-raparthi.github.io"
+con = http.client.HTTPSConnection(url)
+con.request('GET', '/')
+
+response  = con.getresponse()
+print(response.read().decode('utf-8'))
+
+con.close()
+
