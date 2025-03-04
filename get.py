df9c8bdd18f64130a7d41b12644618828c79f602
qb <83409253+kalyan-raparthi@users.noreply.github.com>
2025-03-04 16:09:27 +0530

    get_page() added

--- get.py
+++ get.py
@@ -1,3 +1,17 @@
-import selenium
+from playwright.sync_api import sync_playwright
+
+def get_page(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
+
+        content = page.locator('html').inner_text()
+        browser.close()
+        return content
 
 
+url = 'https://www.livemint.com/news'
+
+doc = get_page(url)
+print(doc)
