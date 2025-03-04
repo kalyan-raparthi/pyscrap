fd969bc586b1deb7f88461f8574ed6b19940b6ed
qb <83409253+kalyan-raparthi@users.noreply.github.com>
2025-03-04 17:11:10 +0530

    Update get.py

--- get.py
+++ get.py
@@ -2,17 +2,20 @@ import re
 from playwright.sync_api import sync_playwright
 
 def get_page(url):
+    print("getting page from" + url)
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(url)
-        content = page.locator('html').inner_text()
+        content = page.locator('p').all_inner_texts()
         browser.close()
         return content
 
-key = input('ENTER KEWORD: ')
+# key = input('ENTER KEWORD: ')
+key = 'bull'
 # urls ===============================================================================================================================
-urls = ['https://www.wikipedia.org/wiki/' + key, 'https://www.britannica.com/search?query=' + key, 'https://www.dictionary.com/browse/' + key, 'https://www.thesaurus.com/browse/' + key]
+urls = ['https://www.wikipedia.org/wiki/' + key, 'https://www.britannica.com/search?query=' + key]
+# 'https://www.dictionary.com/browse/' + key, 'https://www.thesaurus.com/browse/' + key]
 data = []
 
 for url in urls:
@@ -38,5 +41,15 @@ def clean_text(text):
     text = re.sub(r'\s*–\s*', '– ', text) # add space after en dash
     text = re.sub(r'\s*—\s*', '— ', text) # add space after em dash
     text = re.sub(r'\s*…\s*', '… ', text) # add space after ellipsis
-    text = re.sub(r'\s*\(\s*', ' (', text) # add space before opening parenthesis``
+    text = re.sub(r'\s*\(\s*', ' (', text) # add space before opening parenthesis
+    text = re.sub(r'\s*\)\s*', ') ', text) # add space after closing parenthesis
+    text = re.sub(r'\s*\[\s*', ' [', text) # add space before opening square bracket
+    text = re.sub(r'\s*\]\s*', '] ', text) # add space after closing square bracket
+    text = re.sub(r'\s*{\s*', ' {', text) # add space before opening curly brace
+    text = re.sub(r'\s*}\s*', '} ', text) # add space after closing curly brace
     return text
+
+for i in data:
+    print('\n\n\n')
+    for j in i:
+        print(j)
